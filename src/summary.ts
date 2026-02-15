92a1ffaf55a576c938f57471130bf20b54026bee
Ronny Polley <ronnypolley@googlemail.com>
2026-02-15 10:33:03 +0100

    adding first try of report parsing
    # Conflicts:
    #	package-lock.json
    #	package.json

---
+++ src/summary.ts
@@ -0,0 +1,12 @@
+
+export class Summary {
+    private report : Report;
+
+    constructor(report: Report) {
+        this.report = report;
+    }
+
+    public toOverviewSummary() {
+        
+    }
+}
